5cd150488e3025196284d31446bb815cdb268372
SUHAS <suhas@MacBookAir.lan>
2024-11-19 14:52:33 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//importing all the pages
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Game from './components/Game';
+import Puzzles from './components/Puzzles';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/*Setting all the routes of the pages*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/puzzles" element={<Puzzles />} />
+        <Route path="/profile" element={<Profile />} /> 
+      </Routes>
+    </Router>
   );
 }
 
